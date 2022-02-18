51f400f2bd37ab471f4b7038ead5af6deb078793
suzy <2021234@naver.com>
2022-02-18 22:06:45 +0900

    220218-build-1

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 const cors = require('cors')
+app.use(cors());
 
-const port = 5000;
+const connect = require("./schemas");
+connect();
 
+const requestMiddleware = (req, res, next) => {
+    console.log("Request URL:", req.originalUrl, " - ", new Date());
+    next();
+};
 
-app.use(express.urlencoded({ extened: false }));
 app.use(express.json());
+app.use(requestMiddleware);
+
+//router 가져오기
+const contentRouter = require("./routes/content"); 
+const profileRouter = require("./routes/profile"); 
+const userRouter = require("./routes/user"); 
+const videoRouter = require("./routes/video"); 
+
+app.use('/', express.urlencoded({ extended: false }), [
+    contentRouter,
+    profileRouter,
+    userRouter,
+    videoRouter
+]);
+
 
 app.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`);
+    console.log(`listening at http://localhost:${port} : 포트로 서버가 열렸어요!!`);
 });
