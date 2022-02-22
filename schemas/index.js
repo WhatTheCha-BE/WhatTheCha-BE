ce560c0b99ca09b35b5c70f9d2414f82044a1ed9
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-02-23 00:32:36 +0900

    update(schemas index)

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    mongoose.connect(process.env.MONGO_URL, {
+    mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/TheCha', {
         // ignoreUndefined: true
     })
     // mongoose.connect(process.env.MONGO_URL, {
