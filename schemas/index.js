a081cc8b2aedfe4ea5a518057c970c3b572b4567
jableee <kkambo93@naver.com>
2022-02-22 22:01:12 +0900

    commit direct from j

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    mongoose.connect(process.env.MONGO_URL)
+    mongoose.connect(process.env.MONGO_URL, {
+        ignoreUndefined: true
+    })
     // mongoose.connect(process.env.MONGO_URL, {
         // useNewUrlParser: true,
         // useUnifiedTopology: true,
