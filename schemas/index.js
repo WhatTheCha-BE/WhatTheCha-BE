0ac8af34f9bdec76f7c01a0072bc12cce1460c04
jableee <kkambo93@naver.com>
2022-02-22 23:50:56 +0900

    j commit

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = () => {
     mongoose.connect(process.env.MONGO_URL, {
-        ignoreUndefined: true
+        // ignoreUndefined: true
     })
     // mongoose.connect(process.env.MONGO_URL, {
         // useNewUrlParser: true,
