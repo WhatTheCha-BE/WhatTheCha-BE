0f7ae7c73ccd794669e60e5de59458f646549818
jableee <kkambo93@naver.com>
2022-02-24 11:38:38 +0900

    콘솔 확인용 미들웨어 수정

--- middelwares/auth-middleware.js
+++ middelwares/auth-middleware.js
@@ -6,17 +6,21 @@ module.exports = (req, res, next) => {
     const { email } = req.headers;
     console.log('미들웨어: ', authorization, email);
     const [tokenType, tokenValue] = authorization.split(' ');
+    console.log('tokenType: ',tokenType, 'tokenValue: ', tokenValue);
 
     if (tokenType !== 'Bearer') {
         res.status(401).send({
             errorMessage: '로그인 후 사용해주세요.',
         });
+        console.log('tokenType !== Bearer')
         return;
     }
 
+    
     try {
         const { user_id } = jwt.verify(tokenValue, process.env.TOKENKEY);
         User.findById(user_id).exec().then((user) => {
+            console.log('user: ',user)
             res.locals.user = user;
             next();
         });
@@ -24,6 +28,7 @@ module.exports = (req, res, next) => {
         res.status(401).send({
             errorMessage: '로그인 후 사용해주세요!',
         });
+        console.log('err', error);
         return;
     };
 };
